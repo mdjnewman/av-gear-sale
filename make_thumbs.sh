62aff44444625de16e454f78c8f460a66c8a37b2
Matt Newman <mdjnewman@gmail.com>
2015-01-01 23:18:23 +1000

    Improving make_thumbs
    * Now skips generation if the thumbnail exists

--- make_thumbs.sh
+++ make_thumbs.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
 mkdir -p ./assets/images/thumbs
-mogrify -path ./assets/images/thumbs -thumbnail 150x150 ./assets/images/*.*
+cd ./assets/images
+mogrify -path ./thumbs -thumbnail 150x150 $(comm -23 <(ls -1 | grep -v '^thumbs$') <(ls -1 thumbs/))
+
